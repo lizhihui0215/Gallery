42706f557a088d0a5e7aead8915d630e87841760
李智慧 <lizhihui@lizhihuis-MacBook-Pro.local>
2015-10-10 23:32:46 +0800

    add meta for bootstrap

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Gallery App</title>
     <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css"> -->
     <link rel="stylesheet" type="text/css" href="{{ url(elixir('css/site.css')) }}">
